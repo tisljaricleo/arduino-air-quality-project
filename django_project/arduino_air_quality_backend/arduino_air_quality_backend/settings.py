9e383c59adf7ede1c48be8000cbd0d83d668bcb0
Andre Garasic <andre.garasic@gmail.com>
2021-05-06 16:31:21 +0200

    fixes #14 code cleaning using flake8

--- django_project/arduino_air_quality_backend/arduino_air_quality_backend/settings.py
+++ django_project/arduino_air_quality_backend/arduino_air_quality_backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zr_1&xn#qm8pj+oelj$^wv67y2@60*$df$s8)woa4)7dvzd1cv'
+SECRET_KEY = 'django-insecure-zr_1&xn#qm8pj+oelj$^wv67y2@60*$df$s8)woa4' \
+             ')7dvzd1cv '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,16 +88,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 
