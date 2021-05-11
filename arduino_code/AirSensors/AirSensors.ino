ec6cb66c6f3abae6ce4d237a20de879acc31aed4
Leo Tisljaric <tisljaricleo@gmail.com>
2021-05-11 16:32:41 +0200

    fixes #19 Add code for co2 and particles sensors

--- arduino_code/AirSensors/AirSensors.ino
+++ arduino_code/AirSensors/AirSensors.ino
@@ -16,6 +16,7 @@
 #include "DHTesp.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include "MHZ19.h"
 
 #define PINMQ135 13
 #define PINMQ6 25
@@ -25,10 +26,16 @@
 #define DHTTYPE DHT11         // Defines sensor type
 #define WIFISSID "LeoWifi"    // Wifi name
 #define WIFIPSW "wifi12345"   // Wifi password
+#define RXD2 18
+#define TXD2 19
 
 DHTesp dht;                           // Constructor for DHT
 TaskHandle_t tempTaskHandle = NULL;   // Task handle for the light value read task
 Adafruit_SGP30 sgp;                   // Constructor for SGP30
+MHZ19 myMHZ19;
+
+// Main server address
+String hostAdress = "192.168.155.58";
 
 // Sensor readings variables
 float temp = 0.0;
@@ -39,12 +46,29 @@ int sensorMQ135 = 0.0;
 int sensorMQ6 = 0.0;
 int sensorMQ4 = 0.0;
 int sensorMQ7 = 0.0;
+int MHCO2;
+int8_t MHTemp;
+struct pms5003data {//deklaracija uint16_t koji ima vrijednost od 0 do 65,535
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+};
+struct pms5003data data;
 
 // SGP30 must have its baseline values.
 // Sensor must be recalibrated befor usage.
 // Before callibration, sensor must work for at least 24h.
 bool calibrate = false;
 uint16_t TVOC_base, eCO2_base;
+
+// Testing vars
+// Add one sensor with post req
+bool addOne = true;
+
+unsigned long getDataTimer = 0;
 
 /**
    Communication with server using HTTP requests
@@ -84,8 +108,14 @@ void ServerCom(String m, String host, String header = "") {
     if (m == "POST") {
       HTTPClient http;
       http.begin(host);  //Specify destination for HTTP request
-      http.addHeader("Content-Type", "text/plain");             //Specify content-type header
-      int httpResponseCode = http.POST("POSTING from ESP32");   //Send the actual POST request
+      // http.addHeader("Content-Type", "text/plain");             //Specify content-type header
+
+
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST("{\"s_name\":\"arduino1\",\"s_type\":\"MQArduino\",\"s_reading\":\"999999\"}");
+
+      
+      // int httpResponseCode = http.POST("POSTING from ESP32");   //Send the actual POST request
 
       if (httpResponseCode > 0) {
         String response = http.getString();                       //Get the response to the request
@@ -115,6 +145,12 @@ void setup()
   Wire.begin();
   Serial.begin(115200);
   delay(4000);   //Delay needed before calling the WiFi.begin
+
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  myMHZ19.begin(Serial1);
+  myMHZ19.autoCalibration(true);
+  
+  Serial2.begin(9600);//Linija za komunkikaciju s PMS senzorom
 
   pinMode(PINMQ135, INPUT);
   pinMode(PINMQ6, INPUT);
@@ -152,15 +188,26 @@ void setup()
 */
 void loop()
 {
+
+  GetCO2Temp(true);
+
+  if (readPMSdata(&Serial2)) {
+    PlotParticles(true);
+  }
+  
   //  if (calibrate)
   //  {
   //    CalibrateSgp(); // Calibrates SGP sensor
   //  }
   //  calibrate = false;
 
-  ServerCom("GET", "http://jsonplaceholder.typicode.com/comments?id=10", "");
-  ServerCom("POST", "http://jsonplaceholder.typicode.com/posts", "");
+  ServerCom("GET", "http://" + hostAdress + "/api/sensor/", "");
 
+  if (addOne) {
+    ServerCom("POST", "http://" + hostAdress + "/api/sensor/", "");
+    addOne = false;
+  }
+  
   Serial.println("#################################################");
 
   delay(5000);
@@ -172,9 +219,10 @@ void loop()
 
   Serial.println("#################################################");
 
-  delay(3000);
-  // exit(0);
-}
+  delay(2000);
+
+} 
+// END LOOP
 
 /**
    Converts ESP32 dev board readings to voltage (3.3V pins)
@@ -240,11 +288,11 @@ void GetAllSensors(bool plot)
 }
 
 /**
- * Calibrates SGP30 sensor
- * 
- * Sets and prints SGP30 baseline values
- *  
- */
+   Calibrates SGP30 sensor
+
+   Sets and prints SGP30 baseline values
+
+*/
 void CalibrateSgp()
 {
 
@@ -266,12 +314,12 @@ void CalibrateSgp()
 }
 
 /**
- * Gets readings from SGP30 sensor
- * 
- * All values are saved to global variables at the top of this document
- * 
- * @param plot If true, results will be shown in Serial window 
- */
+   Gets readings from SGP30 sensor
+
+   All values are saved to global variables at the top of this document
+
+   @param plot If true, results will be shown in Serial window
+*/
 void GetSgp(bool plot)
 {
   // Check if SGP30 can get sensor reading
@@ -365,3 +413,105 @@ void connectToNetwork() {
     Serial.println("Establishing connection to WiFi..");
   }
   Serial.println("Connected to network");
+}
+
+/**
+   Gets readings from MH-Z19C sensor (CO2 and Temp)
+
+   @param plot True for ploting on console 
+*/
+void GetCO2Temp(bool plot) {
+
+  if (millis() - getDataTimer >= 2000)
+  {
+    
+
+    /* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even
+      if below background CO2 levels or above range (useful to validate sensor). You can use the
+      usual documented command with getCO2(false) */
+
+    MHCO2 = myMHZ19.getCO2();
+    MHTemp = myMHZ19.getTemperature();
+
+    if (plot) {
+      Serial.print("CO2 (ppm): ");
+      Serial.println(MHCO2);
+      Serial.print("Temperature (C): ");
+      Serial.println(MHTemp);
+    }
+    getDataTimer = millis();
+  }
+
+
+}
+
+
+/**
+   Gets readings from PMS5003 sensor
+
+   @return (boolean) True if sensor works.
+*/
+boolean readPMSdata(Stream *s) {
+  if (! s->available()) {
+    return false;
+  }
+
+  //čitaj byte po bit dok ne dobijemo '0x42' startni byte
+  if (s->peek() != 0x42) {
+    s->read();
+    return false;
+  }
+
+  // Sada pročitaj sva 32 bytes
+  if (s->available() < 32) {
+    return false;
+  } uint8_t buffer[32]; uint16_t sum = 0; s->readBytes(buffer, 32);
+
+  // provjerimo sve s kontrolnim zbrojem
+  for (uint8_t i = 0; i < 30; i++) {
+    sum += buffer[i];
+  }
+  // Podatci dolaze u endian'd protokolu, ovaj dio koda omogućava podržanost na svim platformama
+  uint16_t buffer_u16[15];
+  for (uint8_t i = 0; i < 15; i++) {
+    buffer_u16[i] = buffer[2 + i * 2 + 1];
+    buffer_u16[i] += (buffer[2 + i * 2] << 8);
+  }
+
+  // spremimo sve u strukturu
+  memcpy((void *)&data, (void *)buffer_u16, 30);
+
+  if (sum != data.checksum) {
+    Serial.println("Checksum failure");
+    return false;
+  }
+  // kada sve uspije vraćamo rezultat
+  return true;
+}
+
+
+/**
+   Plots readings from PMS5003 sensor
+
+   @param plot True for ploting on console
+*/
+void PlotParticles(bool plot)
+{
+  if (plot)
+  {
+    Serial.println("---------------------------------------");
+    Serial.print("Cestica u zraku > 0.3um / 0.1L zraka:");
+    Serial.println(data.particles_03um);//Čestica većih od 0.3 um
+    Serial.print("Cestica u zraku > 0.5um / 0.1L zraka:");
+    Serial.println(data.particles_05um);//Čestica većih od 0.5 um
+    Serial.print("Cestica u zraku > 1.0um / 0.1L zraka:");
+    Serial.println(data.particles_10um);//Čestica većih od 1 um
+    Serial.print("Cestica u zraku > 2.5um / 0.1L zraka:");
+    Serial.println(data.particles_25um);//Čestica većih od 2.5 um
+    Serial.print("Cestica u zraku > 5.0um / 0.1L zraka:");
+    Serial.println(data.particles_50um);//Čestica većih od 5 um
+    Serial.print("Cestica u zraku > 10.0 um / 0.1L zraka:");
+    Serial.println(data.particles_100um);//Čestica većih od 10 um
+    Serial.println("---------------------------------------");
+  }
+}
